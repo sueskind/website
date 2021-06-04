d767564f3d7ac6e405972d5e1119ff98e2c1cd16
sueskind <52210599+sueskind@users.noreply.github.com>
2021-06-04 15:47:32 +0200

    Add script to generate album description templates

---
+++ generate-images/generate-description-template.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3.9
+
+import argparse
+import os
+import json
+
+parser = argparse.ArgumentParser("Create a description.json template for all images in target.")
+parser.add_argument("target", help="Target path where the images are.")
+
+args = parser.parse_args()
+target = args.target
+
+files = {f: "" for f in os.listdir(target)}
+
+with open("descriptions.json", "w") as f:
+    json.dump(files, f, indent=4, sort_keys=True)

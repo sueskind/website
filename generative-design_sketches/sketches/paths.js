acb00851c6c46c040895eab1dbb38bc7d83b1f66
sueskind <52210599+sueskind@users.noreply.github.com>
2021-07-02 17:30:45 +0200

    Add margin

--- generative-design_sketches/sketches/paths.js
+++ generative-design_sketches/sketches/paths.js
@@ -1,13 +1,17 @@
 let points;
 let indices;
-let origin;
+let mainPoint;
 
 const count = 6;
 const frames = 10;
 
-const lineWidth = 10;
-const mainCircleSize = 80;
-const circleSize = 40;
+let lineWidth = 0.5;
+let mainCircleSize = 4;
+let circleSize = 2;
+
+const margin = 30;
+let drawAreaWidth;
+let drawAreaHeight;
 
 let frameWidth;
 let frameHeight;
@@ -19,8 +23,15 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
     noLoop();
 
-    frameWidth = int(width / frames);
-    frameHeight = int(height / frames);
+    drawAreaWidth = width - 2 * margin;
+    drawAreaHeight = height - 2 * margin;
+
+    frameWidth = int(drawAreaWidth / frames);
+    frameHeight = int(drawAreaHeight / frames);
+
+    lineWidth /= min(frameWidth, frameHeight);
+    mainCircleSize /= min(frameWidth, frameHeight);
+    circleSize /= min(frameWidth, frameHeight);
 
     strokeWeight(lineWidth);
     fill(255);
@@ -30,7 +41,7 @@ function setup() {
     button_reset.position(20, 20);
     button_reset.mousePressed(draw);
 
-    origin = {"x": width / 2, "y": 20};
+    mainPoint = {"x": 0.5, "y": 0.05};
 }
 
 function draw() {
@@ -38,33 +49,36 @@ function draw() {
 
     points = [];
     for (let i = 0; i < count / 2; i++) {
-        let x = random() * width / 2;
-        let y = random() * height;
-        points.push({"x": width / 2 + x, "y": y});
-        points.push({"x": width / 2 - x, "y": y});
+        let x = random() / 2;
+        let y = random();
+        points.push({"x": 0.5 + x, "y": y});
+        points.push({"x": 0.5 - x, "y": y});
     }
 
-    scale(1.0 / frames);
+    push();
+    translate(margin, margin);
+    //circle(0,0,30);
+    scale(frameWidth, frameHeight);
 
     for (let r = 0; r < frames; r++) {
         for (let c = 0; c < frames; c++) {
 
-            line(origin.x, origin.y, points[0].x, points[0].y);
+            line(mainPoint.x, mainPoint.y, points[0].x, points[0].y);
             for (let i = 0; i < count - 1; i++) {
                 line(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
             }
 
-            circle(origin.x, origin.y, mainCircleSize);
+            circle(mainPoint.x, mainPoint.y, mainCircleSize);
             for (let i = 0; i < count; i++) {
                 circle(points[i].x, points[i].y, circleSize);
             }
 
             points = shuffle(points);
 
-            translate(frameWidth * frames, 0);
+            translate(1, 0);
         }
-        translate(-frameWidth * frames * frames, frameHeight * frames);
+        translate(-frames, 1);
     }
 
-    resetMatrix();
+    pop();
 }

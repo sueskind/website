dd21b8bc519267e2f2d30d563bb8fb4c3700d39f
sueskind <52210599+sueskind@users.noreply.github.com>
2021-07-31 23:09:18 +0200

    Add other digits

--- generative-design_sketches/sketches/clock.js
+++ generative-design_sketches/sketches/clock.js
@@ -1,3 +1,8 @@
+let spaceSmall = 10;
+let spaceBig = 30;
+let barWidth = 50;
+let circleRadius = 25;
+
 let numberFont;
 
 function preload() {
@@ -14,23 +19,42 @@ function setup() {
 }
 
 function draw() {
+    translate(width / 2, height / 2);
     background(230);
-    translate(width / 2, height / 2 - (frameCount % 5) * 50);
-    stripe(6);
+
+    stripe(-(spaceSmall * 1.5 + barWidth * 3 + spaceBig), 3, frameCount % 3);
+    stripe(-(spaceSmall / 2 + barWidth * 2 + spaceBig), 10, frameCount % 10);
+    stripe(-(spaceSmall / 2 + barWidth), 6, frameCount % 6);
+    stripe(spaceSmall / 2, 10, frameCount % 10);
+    stripe(spaceSmall / 2 + barWidth + spaceBig, 6, frameCount % 6);
+    stripe(spaceSmall * 1.5 + barWidth * 2 + spaceBig, 10, frameCount % 10);
 
     fill(0, 0, 0, 0);
     strokeWeight(3);
-    circle(25, 25 + (frameCount % 5) * 50, 50);
+
+    circle(-(spaceSmall * 1.5 + spaceBig + barWidth * 2 + circleRadius), 25, 50);
+    circle(-(spaceSmall / 2 + spaceBig + barWidth + circleRadius), 25, 50);
+    circle(-(spaceSmall / 2 + circleRadius), 25, 50);
+    circle(spaceSmall / 2 + circleRadius, 25, 50);
+    circle(spaceSmall / 2 + spaceBig + barWidth + circleRadius, 25, 50);
+    circle(spaceSmall * 1.5 + spaceBig + barWidth * 2 + circleRadius, 25, 50);
 }
 
-function stripe(num) {
+function stripe(x, length, num) {
+    push();
+
+    translate(x, -6);
+
+    translate(0, -(num % length) * 50);
     fill(230);
     strokeWeight(4);
-    rect(0, 0, 50, 50 * num + 15);
+    rect(0, 0, 50, 50 * length + 15);
 
     fill(0);
     strokeWeight(1);
-    for (let i = 0; i < num; i++) {
+    for (let i = 0; i < length; i++) {
         text(i, 25, i * 50 + 25);
     }
+
+    pop();
 }

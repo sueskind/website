592cd9ea8586584b00914fd14fac125983086ee2
sueskind <52210599+sueskind@users.noreply.github.com>
2021-08-01 18:04:26 +0200

    Colorize

--- generative-design_sketches/sketches/clock.js
+++ generative-design_sketches/sketches/clock.js
@@ -8,6 +8,9 @@ let majorFontSize = 36;
 let fontSize = 28;
 
 let backgroundColor = 230;
+let colorSaturation = 30;
+let colorRange = 30;
+let colorStart;
 
 let numberFont;
 
@@ -17,23 +20,30 @@ function preload() {
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    frameRate(30);
+
     stroke(0);
+    colorMode(HSB, 100);
+
     textFont(numberFont);
     textSize(fontSize);
     textAlign(CENTER, CENTER);
-    frameRate(30);
+
+    colorStart = random(0, 100);
 }
 
 function draw() {
     translate(width / 2, height / 2);
     background(backgroundColor);
 
-    stripe(-(spaceSmall * 1.5 + barWidth * 3 + spaceBig), 3, Math.floor(hour() / 10));
-    stripe(-(spaceSmall / 2 + barWidth * 2 + spaceBig), 10, hour() % 10);
-    stripe(-(spaceSmall / 2 + barWidth), 6, Math.floor(minute() / 10));
-    stripe(spaceSmall / 2, 10, minute() % 10);
-    stripe(spaceSmall / 2 + barWidth + spaceBig, 6, Math.floor(second() / 10));
-    stripe(spaceSmall * 1.5 + barWidth * 2 + spaceBig, 10, second() % 10);
+    let time = ((hour() * 60 + minute()) * 60 + second()) * 1000 + new Date().getMilliseconds();
+
+    stripe(-(spaceSmall * 1.5 + barWidth * 3 + spaceBig), 3, Math.floor(hour() / 10), (time / 1000 / 60 / 60 / 10 % 3) / 3);
+    stripe(-(spaceSmall / 2 + barWidth * 2 + spaceBig), 10, hour() % 10, (time / 1000 / 60 / 60 % 10) / 10);
+    stripe(-(spaceSmall / 2 + barWidth), 6, Math.floor(minute() / 10), (time / 1000 / 60 / 10 % 6) / 6);
+    stripe(spaceSmall / 2, 10, minute() % 10, (time / 1000 / 60 % 10) / 10);
+    stripe(spaceSmall / 2 + barWidth + spaceBig, 6, Math.floor(second() / 10), (time / 1000 / 10 % 6) / 6);
+    stripe(spaceSmall * 1.5 + barWidth * 2 + spaceBig, 10, second() % 10, (time / 1000 % 10) / 10);
 
     fill(0, 0, 0, 0);
     strokeWeight(3);
@@ -46,13 +56,15 @@ function draw() {
     circle(spaceSmall * 1.5 + spaceBig + barWidth * 2 + circleRadius, 25, 50);
 }
 
-function stripe(x, length, num) {
+function stripe(x, length, num, progress) {
     push();
 
     translate(x, -6);
 
     translate(0, -(num % length) * barWidth);
-    fill(backgroundColor);
+
+    let hue = (progress * colorRange + colorStart) % 100;
+    fill(hue, colorSaturation, 100);
     strokeWeight(4);
     rect(0, 0, barWidth, barWidth * length + 15, borderRadius);
 
